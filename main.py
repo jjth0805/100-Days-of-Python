40f716c7531fb0bd7c3cb704cb9dd675f4ea01de
TaehyunJeong <>
2021-05-06 13:04:39 +0000

    Day 6,7 hangman game /game/ハングマンゲーム

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import Day2.tip_cal, Day3.treasure_island_game, Day4.matrix, Day5.random_pw_gen
+import Day2.tip_cal, Day3.treasure_island_game, Day4.matrix, Day5.random_pw_gen, Day6and7.hangman
 # Day2.tip_cal.tip_calculate()
 # Day3.treasure_island_game.treasure_game()
 # Day4.matrix.matrix_run()
-Day5.random_pw_gen.pw_gen()
+# Day5.random_pw_gen.pw_gen()
+Day6and7.hangman.hangman_start()
