c48818774764dd7f94e3f4c2d8d2caa70b0d0ab7
TaehyunJeong <>
2021-05-06 12:26:54 +0000

    Day5 password generator/ランダム暗証番号作り

---
+++ main.py
@@ -0,0 +1,5 @@
+import Day2.tip_cal, Day3.treasure_island_game, Day4.matrix, Day5.random_pw_gen
+# Day2.tip_cal.tip_calculate()
+# Day3.treasure_island_game.treasure_game()
+# Day4.matrix.matrix_run()
+Day5.random_pw_gen.pw_gen()
